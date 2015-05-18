e6cd43e0d8b3b7a1496d86732e52158c72a0a137
maheshkkolla <mahesh.mk.245@gmail.com>
2015-05-18 17:41:36 +0530

    [Mahesh] Domains(collection of accounts)  added to DB

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,10 +1,12 @@
 var DataStore = require('nedb');
 var gui = require('nw.gui');
-var db;
+var db = {};
+db.domains = new DataStore({filename: './db/domains.json', autoload: true }); 
+db.accounts = new DataStore({filename: './db/accounts.json', autoload: true }); 
 
 var checkForSetUpOrLogin = function() {
-	db = new DataStore({filename: './db/accounts.json', autoload: true }); 
-	db.find({_id:1}, function(err, thisAccount) {
+	
+	db.domains.find({_id:1}, function(err, thisAccount) {
 		err && alert("Error: "+err);
 		if(thisAccount.length == 0) $('#signUpModal').modal('show');
 		else $("#loginModal").modal('show');
@@ -27,20 +29,23 @@ var setPassword = function() {
 	var confirmPassword = $("#confirmPassword").val();
 	if(validateSetPassword(password,confirmPassword)){
 		var encrptedPassword = new Buffer(password).toString('base64');
-		var record = {_id:1, account:"Password Manger", userName:"", password:encrptedPassword};
-		db.insert(record, function(err, result) {
-			err && alert("Error at inserting"+err);
-			$('#signUpModal').modal('hide');
-			$('#loginModal').modal('show');
+		var domainRecord = {_id:1, domain:"Password Manger"};
+		var accountRecord = {_id:1, account:"Desktop App", description:"",userName:"",password:encrptedPassword,domainId:1};
+		db.domains.insert(domainRecord, function(domainError, result) {
+			domainError && alert("Error at inserting "+JSON.stringify(domainError));
+			db.accounts.insert(accountRecord, function(accountError,row) {
+				accountError && alert("Error at inserting "+JSON.stringiFy(accountError));
+				$('#signUpModal').modal('hide');
+				$('#loginModal').modal('show');
+			});
 		});		
 	}
 }
 
 var doLogin = function() {
 	var password = $("#password").val();
-	db.find({_id:1}, function(err, record) {
+	db.accounts.find({_id:1}, function(err, record) {
 		err && alert("Error:"+err);
-		console.log("Pas:",password,"re:",record);
 		var encrptedPassword = new Buffer(password).toString('base64');
 		if(record[0].password == encrptedPassword){
 			gui.Window.open("./index.html");
