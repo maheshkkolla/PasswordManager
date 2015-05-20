94a522e7bddea0503ae7d7dbf3e84b9448c9c5d8
maheshkkolla <mahesh.mk.245@gmail.com>
2015-05-20 16:21:26 +0530

    [Mahesh] Displaying domain names in the list

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-var accountTemplate = fs.readFileSync('./account.template', 'utf-8');
-var domainTemplate = fs.readFileSync('./domain.template', 'utf-8');
+var accountTemplate = fs.readFileSync('./templates/account.template', 'utf-8');
+var domainTemplate = fs.readFileSync('./templates/domain.template', 'utf-8');
 var gui = require('nw.gui');
 var DataStore = require('nedb');
 
@@ -9,8 +9,23 @@ db.domains = new DataStore({filename: './db/domains.json', autoload: true });
 db.accounts = new DataStore({filename: './db/accounts.json', autoload: true }); 
 
 
+var displayDomainInList = function(domain) {
+	var template = domainTemplate;
+	template = template.replace("@ID@",domain._id);
+	template = template.replace("@NAME@",domain.name);
+	$("#domains").append(template);
+}
+
+var loadDomainsList = function() {
+	db.domains.find({}, function(domainError, domains) {
+		domainError && alert("Error: "+domainError);
+		domains.forEach(displayDomainInList);
+	});
+}
+
 var documentReady = function() {
 	gui.Window.get().setMinimumSize(1000,500);
+	loadDomainsList();
 }
 
 $(document).ready(documentReady);
